2196c95ed74070fcac4ae697b261042edb9527b1
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-27 15:03:55 +0200

    fixing scss and adding modules to Plutone

--- src/components/Shape/index.js
+++ src/components/Shape/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styles from "./Shape.module.scss";
 
 function index() {
   return (
     <div>
-      <p>I'm a shape</p>
+      <p className={styles.color}>I'm a shape that is using css modules</p>
     </div>
   );
 }
