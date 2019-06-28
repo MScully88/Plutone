7acb669533b2052b3267eda6444f506455bc9823
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-28 11:34:24 +0200

    trialling initial keys for sound, setting up hooks

--- src/components/Shape/index.js
+++ src/components/Shape/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tone from 'tone';
 import styles from './Shape.module.scss';
 
-function index() {
+const index = () => {
+  const keys = () => {
+    const synth = new Tone.Synth().toMaster();
+    // play a middle 'C' for the duration of an 8th note
+    const loop = new Tone.Loop(time => {
+      synth.triggerAttackRelease('C2', '8n', time);
+    }, '4n');
+    loop.start('0m').stop('4m');
+    Tone.Transport.start();
+  };
   return (
     <div>
+      <button type="button" onClick={keys}>
+        Fire Keys
+      </button>
       <p className={styles.color}>I'm a shape that is using css modules</p>
     </div>
   );
-}
+};
 
 export default index;
