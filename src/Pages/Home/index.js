22771e9b2af73569f2c93e77ad0452262128246c
MScully88 <michael.scully88@gmail.com>
2019-07-02 17:28:56 +0200

    Homepage BEM Styling

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,11 +5,13 @@ import styles from './Home.module.scss';
 const Home = () => {
   return (
     <>
-      <div className={styles.home}>
-        <h3>P L U T O N E | Welcome</h3>
+      <div className={styles.block}>
+        <h3 className={styles.block__elem__title}>P L U T O N E | Welcome</h3>
       </div>
-      <div className={styles.home}>
-        <Link to="/Plutone">E N T E R</Link>
+      <div className={styles.block}>
+        <Link className={styles.block__elem__links} to="/Plutone">
+          E N T E R
+        </Link>
       </div>
     </>
   );
