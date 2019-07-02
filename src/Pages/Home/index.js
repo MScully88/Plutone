c158d39b968b17de69497e98ef6646009f187c95
MScully88 <michael.scully88@gmail.com>
2019-07-02 14:51:38 +0200

    Home styling

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Home.module.scss';
 
 const Home = () => {
   return (
-    <div className="App">
-      <p>This is the home page</p>
-      <Link to="/Plutone">Click me</Link>
-    </div>
+    <>
+      <div className={styles.home}>
+        <h3>P L U T O N E | Welcome</h3>
+      </div>
+      <div className={styles.home}>
+        <Link to="/Plutone">E N T E R</Link>
+      </div>
+    </>
   );
 };
 
