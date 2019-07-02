5d19a627581a974372c7b9d5e9aecf4229265eb3
MScully88 <michael.scully88@gmail.com>
2019-07-02 17:29:31 +0200

    Loading Page BEM Styling

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,15 +3,15 @@ import styles from './Loading.module.scss';
 
 function Loading() {
   return (
-    <div className={styles.splashContainer}>
-      <div className={styles.h1}>
-        <h1 data-text="L O A D I N G">L O A D I N G</h1>
-      </div>
-      <div className={styles.loading}>
-        <div className={styles.loadingBar} />
-        <div className={styles.loadingBar} />
-        <div className={styles.loadingBar} />
-        <div className={styles.loadingBar} />
+    <div className={styles.block}>
+      <h1 className={styles.block__elem__title} data-text="L O A D I N G">
+        L O A D I N G
+      </h1>
+      <div className={styles.block__elem__loadBars}>
+        <div className={styles.block__elem__loadingBar} />
+        <div className={styles.block__elem__loadingBar} />
+        <div className={styles.block__elem__loadingBar} />
+        <div className={styles.block__elem__loadingBar} />
       </div>
     </div>
   );
