a8c24eab28e36cda2a56a43a965d4ffc4f63d89d
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 11:14:53 +0200

    create array of inst

--- src/Pages/Plutone/index.js
+++ src/Pages/Plutone/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Plutone.module.scss';
+import Shape from '../../components/Shape';
 
-const Plutone = () => {
-  return <div className={styles.block} />;
+const Plutone = ({ stems }) => {
+  const [instArray, setinstArray] = useState(['bassMain', 'synthStr', 'keysMain']);
+
+  return (
+    <div className={styles.block}>
+      {instArray.map((inst, index) => {
+        return <Shape key={index} stems={stems} instArray={inst[index]} />;
+      })}
+    </div>
+  );
 };
 
 export default Plutone;
