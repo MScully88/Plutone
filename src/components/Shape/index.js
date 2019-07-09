fbfacbcf13d4ccd568cff85a7d794a444a0c50b9
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 12:00:09 +0200

    initialising shards

--- src/components/Shape/index.js
+++ src/components/Shape/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import styles from './Shape.module.scss';
 
-function index({ stems, instArray }) {
-  console.log(stems, instArray);
+function index({ songStems, instArray }) {
+  console.log('shape', songStems, instArray);
   return (
     <div>
       <button className={styles.color}>I'm a shape that is using css modules</button>
