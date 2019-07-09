8214bfedb543eeb715601414a0fa478a5b04e851
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 11:15:07 +0200

    getting props from appjs

--- src/components/Shape/index.js
+++ src/components/Shape/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Shape.module.scss';
 
-function index() {
+function index({ stems, instArray }) {
+  console.log(stems, instArray);
   return (
     <div>
-      <p className={styles.color}>I'm a shape that is using css modules</p>
+      <button className={styles.color}>I'm a shape that is using css modules</button>
     </div>
   );
 }
