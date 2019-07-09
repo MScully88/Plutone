c3528dbfe368eb083108121bb9ce0d5034d48b7d
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 11:59:47 +0200

    mapping over array to create components

--- src/Pages/Plutone/index.js
+++ src/Pages/Plutone/index.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import styles from './Plutone.module.scss';
 import Shape from '../../components/Shape';
 
-const Plutone = ({ stems }) => {
+const Plutone = ({ songStems }) => {
+  console.log('plunton', songStems);
   const [instArray, setinstArray] = useState(['bassMain', 'synthStr', 'keysMain']);
 
   return (
     <div className={styles.block}>
       {instArray.map((inst, index) => {
-        return <Shape key={index} stems={stems} instArray={inst[index]} />;
+        return <Shape key={index} songStems={songStems} instArray={instArray} />;
       })}
     </div>
   );
