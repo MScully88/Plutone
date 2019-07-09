d3a46acf0cf12add62c349bc94ea7d50f8e48d2b
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 11:14:14 +0200

    intitiating player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Tone from 'tone';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReactTimeout from 'react-timeout';
+import bassMain from './Sounds/Plutone_V1_04072019 Bass_Main.wav';
+import synthStr from './Sounds/Plutone_V1_04072019 SynthStr_Main.wav';
+import keysMain from './Sounds/Plutone_V1_04072019 Keys_Main.wav';
 import Home from './Pages/Home';
 import Plutone from './Pages/Plutone';
 import Loading from './components/Loading';
@@ -9,13 +13,30 @@ import './App.module.scss';
 // const Plutone = lazy(() => import('./Pages/Plutone'));
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true); // false
+  const [songStems, setSongStems] = useState(null);
+
+  const trackLoad = trackObj => {
+    setSongStems(trackObj);
+    // tracks.get(songStems[1]).start();
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-  }, []);
+    const tracks = new Tone.Players(
+      {
+        bassMain,
+        synthStr,
+        keysMain,
+      },
+      {
+        volume: -3,
+        onload: () => {
+          setLoading(false);
+        },
+      },
+    ).toMaster();
+    trackLoad(tracks);
+  });
 
   return (
     <Router>
@@ -24,7 +45,10 @@ const App = () => {
       ) : (
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/Plutone" component={Plutone} />
+          <Route
+            path="/Plutone"
+            render={routeProps => <Plutone {...routeProps} songStems={songStems} />}
+          />
         </Switch>
       )}
     </Router>
