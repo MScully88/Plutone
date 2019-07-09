d877c4fc48968c6d06b09f9652eb3c3adbc1261f
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-09 15:32:52 +0200

    changing loading naming

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,20 +3,20 @@ import styles from './Loading.module.scss';
 
 function Loading() {
   return (
-    <div className={styles.block}>
-      <h1 className={styles.block__elem__title} data-text="L O A D I N G">
+    <div className={styles.loadingContainer}>
+      <h1 className={styles.loadingContainer__title} data-text="L O A D I N G">
         L O A D I N G
       </h1>
-      <div className={styles.block__elem__loadBars}>
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
-        <div className={styles.block__elem__loadingBar} />
+      <div className={styles.loadingContainer__loadBars}>
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
+        <div className={styles.loadingContainer__loadingBar} />
       </div>
     </div>
   );
