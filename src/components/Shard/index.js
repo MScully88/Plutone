c7d88e13df82f3a07caaad2cd150d117f8ab8e5d
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-10 15:54:30 +0200

    adding onclick functionality to bring in track

--- src/components/Shard/index.js
+++ src/components/Shard/index.js
@@ -1,17 +1,26 @@
 import React, { useEffect } from 'react';
+import Tone from 'tone';
 import PropTypes from 'prop-types';
 import styles from './Shard.module.scss';
 
 const Shard = ({ shardTrack, instArray, instIndex }) => {
+  const playSoundOnClick = () => {
+    const vol = new Tone.Volume(0);
+    shardTrack.get(instArray[instIndex]).chain(vol, Tone.Master);
+  };
+
   useEffect(() => {
-    shardTrack.get(instArray[instIndex]).start();
+    const track = shardTrack.get(instArray[instIndex]);
+    track.start();
   }, [shardTrack, instArray, instIndex]);
 
   return (
     <>
-      <button type="button" className={styles.color}>
-        I'm a shape that is using css modules
-      </button>
+      <div>
+        <button type="button" className={styles.color} onClick={playSoundOnClick}>
+          I'm {instArray[instIndex]}
+        </button>
+      </div>
     </>
   );
 };
