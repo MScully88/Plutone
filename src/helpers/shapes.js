d4cab55d57878b3086b028668b568e12c18d5679
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-26 17:37:37 +0200

    initial path data to bring into react

---
+++ src/helpers/shapes.js
@@ -0,0 +1,319 @@
+const kickMain = [
+  // center
+  {
+    name: 'kickMain',
+    x: 290,
+    y: 300,
+    data:
+      'M65.0179 127.813L153 149.5L120.295 103.186L119.136 102.301L141 84.5714L110.858 74.8431L121.884 54.496L153 1.50001L109.05 28.3302L119.136 12L83.1708 22.4739L2.50015 1.50001L43.2777 43.4215L16.0002 44.6604L22.3561 80.3991L53.1727 88.9959L2.50015 115L43.1574 103.806L22.3561 149.5L65.0179 127.813Z',
+    fill: '#AED9DC',
+    stroke: 'black',
+    strokeWidth: 1,
+    scale: {
+      x: 1,
+      y: 1,
+    },
+  },
+  {
+    x: 290,
+    y: 300,
+    data:
+      'M83.1708 74.8431L83.1708 22.4739M83.1708 74.8431L57.1211 44.6604M83.1708 74.8431L53.1727 88.9959M83.1708 74.8431L121.884 54.496M83.1708 74.8431L98.9643 44.6604M83.1708 74.8431L72.0961 51.4177L57.1211 44.6604M83.1708 74.8431L57.1211 74.8431M83.1708 74.8431L119.136 102.301M83.1708 74.8431L76.6224 93.9512M83.1708 22.4739L119.136 12L109.05 28.3302M83.1708 22.4739L2.50015 1.50001M57.1211 44.6604L2.50015 1.50001M53.1727 88.9959L22.3561 80.3991L16.0002 44.6604M53.1727 88.9959L2.50015 115L43.1574 103.806M121.884 54.496L110.858 74.8431L141 84.5714L119.136 102.301M121.884 54.496L98.9643 44.6604M121.884 54.496L153 1.50001L109.05 28.3302M98.9643 44.6604L109.05 28.3302M57.1211 74.8431L16.0002 44.6604M57.1211 74.8431L43.2777 43.4215M65.0179 127.813L83.1708 93.9512L153 149.5M65.0179 127.813L153 149.5M65.0179 127.813L76.6224 93.9512M65.0179 127.813L22.3561 149.5L43.1574 103.806M153 149.5L120.295 103.186L119.136 102.301M2.50015 1.50001L43.2777 43.4215M16.0002 44.6604L43.2777 43.4215M76.6224 93.9512L43.1574 103.806',
+    stroke: 'black',
+    strokeWidth: 0.5,
+    scale: {
+      x: 1,
+      y: 1,
+    },
+  },
+];
+
+const drumsMain = [
+  // top
+  {
+    name: 'drumsMain',
+    x: 300,
+    y: 41,
+    data:
+      'M81.8874 113.377L91 20.8142L75.7122 4.43327L69.123 43.8202L56.1014 28.5999L56.8631 17.042L40.432 14.928L35.5688 113.377L40.432 1L0.999986 14.928L27.6366 139.036L48.3643 146L81.8874 113.377Z',
+    fill: '#C4C4C4',
+    stroke: 'black',
+    strokeWidth: 1,
+    scale: {
+      x: 1,
+      y: 1,
+    },
+  },
+  {
+    x: 300,
+    y: 41,
+    data:
+      'M59.9321 132.611L69.123 43.8202M69.123 43.8202L75.7122 4.43327L91 20.8142L81.8874 113.377L48.3643 146M69.123 43.8202L56.1014 28.5999L48.3643 146M48.3643 146L56.8631 17.042L40.432 14.928L35.5688 113.377L40.432 1L0.999988 14.928L27.6366 139.036L48.3643 146Z',
+    stroke: 'black',
+    strokeWidth: 1,
+    scale: {
+      x: 1,
+      y: 1,
+    },
+  },
+];
+
+// // Left shard
+
+// let leftLayer = new Konva.Layer();
+
+// let leftSvg101 = new Konva.Path({
+//   x: 25,
+//   y: 300,
+//   data:
+//     "M155.857 62.8703L154.557 57.6885L145.879 55.9776L147.547 51.6697L139.027 30.8197L11.0281 33.4536L29.2094 50.6592L48.0831 50.0616L47.2443 53.6104L17.318 62.2276L10.7827 74.9469L23.8305 76.9259L36.4845 99.3518L104.228 87.1073L148.916 79.034L140.307 69.1444L156.511 65.4769L155.857 62.8703Z",
+//   fill: "#C4C4C4",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+// let leftSvg102 = new Konva.Path({
+//   x: 25,
+//   y: 300,
+//   data:
+//     "M147.547 51.6697L123.255 50.022L51.0022 37.7115L48.0831 50.0616M147.547 51.6697L145.879 55.9776M147.547 51.6697L139.027 30.8197L11.0281 33.4536L29.2094 50.6592L48.0831 50.0616M46.1286 58.3304L44.1826 66.5637L144.21 60.2854L145.879 55.9776M46.1286 58.3304L36.5905 60.7244L41.0282 78.4053M46.1286 58.3304L47.2443 53.6104M155.857 62.8703L42.2106 83.116L41.0282 78.4053M155.857 62.8703L154.557 57.6885L145.879 55.9776M155.857 62.8703L156.511 65.4769L140.307 69.1444M47.2443 53.6104L17.318 62.2276L10.7827 74.9469L23.8305 76.9259M47.2443 53.6104L48.0831 50.0616M41.0282 78.4053L32.2129 77.3973M32.2129 77.3973L44.1654 90.9043L140.307 69.1444M32.2129 77.3973L23.8305 76.9259M140.307 69.1444L148.916 79.034L104.228 87.1073L36.4845 99.3518L23.8305 76.9259",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+// // Bottom shard
+
+// let bottomLayer = new Konva.Layer();
+
+// let bottomSvg101 = new Konva.Path({
+//   x: 320,
+//   y: 580,
+//   data:
+//     "M26.4733 5.82519L13.3637 101.404L22.9373 104.686L36.9261 6.79102L18.3877 138.853L28.4725 140.171L42.5764 171.448L63.3494 144.238L70.1781 58.6758L57.4015 57.3522L63.5475 9.25082L55.6975 8.52549L36.9261 6.79102L26.4733 5.82519Z",
+//   fill: "#C4C4C4",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+// let bottomSvg102 = new Konva.Path({
+//   x: 320,
+//   y: 580,
+//   data:
+//     "M55.6975 8.52549L43.179 101.971M43.179 101.971L38.5572 141.489L28.4725 140.171M43.179 101.971L51.2556 105.454L57.4015 57.3522M28.4725 140.171L18.3877 138.853L36.9261 6.79102L22.9373 104.686L13.3637 101.404L26.4733 5.82519L63.5475 9.25082L57.4015 57.3522M28.4725 140.171L42.5764 171.448L63.3494 144.238L70.1781 58.6758L57.4015 57.3522",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+// // Right shard
+
+// let rightLayer = new Konva.Layer();
+
+// let rightSvg101 = new Konva.Path({
+//   x: 530,
+//   y: 345,
+//   data:
+//     "M1 14.9877L1 20.3302L9.00044 24.1023L6.33363 27.8744L9.52105 50.1712L134.311 78.7771L120.865 57.663L102.414 53.648L104.091 50.4101L135.215 49.3375L144.65 38.5918L132.476 33.496L125.663 8.66419L56.9765 4.04866L11.6673 1.00004L17.6094 12.6879L1 12.3002L1 14.9877Z",
+//   fill: "#C4C4C4",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+// let rightSvg102 = new Konva.Path({
+//   x: 530,
+//   y: 345,
+//   data:
+//     "M6.33363 27.8744L29.4937 35.3863L96.5757 64.9158L102.414 53.648M6.33363 27.8744L9.00044 24.1023M6.33363 27.8744L9.52105 50.1712L134.311 78.7771L120.865 57.663L102.414 53.648M106.322 46.1037L110.214 38.5918L11.6673 20.3302L9.00044 24.1023M106.322 46.1037H116.156V27.8744M106.322 46.1037L104.091 50.4101M1 14.9877L116.156 23.0176V27.8744M1 14.9877L1 20.3302L9.00044 24.1023M1 14.9877L1 12.3002L17.6094 12.6879M104.091 50.4101L135.215 49.3375L144.65 38.5918L132.476 33.496M104.091 50.4101L102.414 53.648M116.156 27.8744L124.461 30.9981M124.461 30.9981L116.156 14.9877L17.6094 12.6879M124.461 30.9981L132.476 33.496M17.6094 12.6879L11.6673 1.00004L56.9765 4.04866L125.663 8.66419L132.476 33.496",
+//   stroke: "black",
+//   strokeWidth: 1,
+//   scale: {
+//     x: 1,
+//     y: 1
+//   }
+// });
+
+export { kickMain, drumsMain };
+
+// // Circular Movement Functionality
+
+// let circleFunc1 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     console.log(pos);
+//     let x = 0;
+//     let y = 0;
+//     let radius = 50;
+//     let scale =
+//       radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+//     if (scale < 1)
+//       return {
+//         y: Math.round((pos.y - y) * scale + y),
+//         x: Math.round((pos.x - x) * scale + x)
+//       };
+//     else return pos;
+//   }
+// });
+
+// let circleFunc2 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     console.log(pos);
+//     let x = 0;
+//     let y = 0;
+//     let radius = 50;
+//     let scale =
+//       radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+//     if (scale < 1)
+//       return {
+//         y: Math.round((pos.y - y) * scale + y),
+//         x: Math.round((pos.x - x) * scale + x)
+//       };
+//     else return pos;
+//   }
+// });
+
+// let circleFunc3 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     console.log(pos);
+//     let x = 0;
+//     let y = 0;
+//     let radius = 50;
+//     let scale =
+//       radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+//     if (scale < 1)
+//       return {
+//         y: Math.round((pos.y - y) * scale + y),
+//         x: Math.round((pos.x - x) * scale + x)
+//       };
+//     else return pos;
+//   }
+// });
+
+// let circleFunc4 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     console.log(pos);
+//     let x = 0;
+//     let y = 0;
+//     let radius = 50;
+//     let scale =
+//       radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+//     if (scale < 1)
+//       return {
+//         y: Math.round((pos.y - y) * scale + y),
+//         x: Math.round((pos.x - x) * scale + x)
+//       };
+//     else return pos;
+//   }
+// });
+
+// // Horizontal Movement Functionality
+
+// let horizontalFunc1 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     return {
+//       x: pos.x,
+//       y: this.absolutePosition().y
+//     };
+//   }
+// });
+
+// let horizontalFunc2 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     return {
+//       x: pos.x,
+//       y: this.absolutePosition().y
+//     };
+//   }
+// });
+
+// // Vertical Movement Functionality
+
+// let verticalFunc1 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     return {
+//       x: this.absolutePosition().x,
+//       y: pos.y
+//     };
+//   }
+// });
+
+// let verticalFunc2 = new Konva.Group({
+//   draggable: true,
+//   dragBoundFunc: function(pos) {
+//     return {
+//       x: this.absolutePosition().x,
+//       y: pos.y
+//     };
+//   }
+// });
+
+// // Functionality added to each layer
+
+// fx1Layer.add(circleFunc1);
+// fx2Layer.add(circleFunc2);
+// fx3Layer.add(circleFunc3);
+// fx4Layer.add(circleFunc4);
+// leftLayer.add(horizontalFunc1);
+// topLayer.add(verticalFunc1);
+// bottomLayer.add(verticalFunc2);
+// rightLayer.add(horizontalFunc2);
+
+// // Centre SVG path added to center layer
+
+// centerLayer.add(centerSvg101, centerSvg102);
+
+// // Each SVG path added to the functionality
+
+// circleFunc1.add(fxSvg101, fxSvg102, fxSvg103, fxSvg104);
+
+// circleFunc2.add(fxSvg201, fxSvg202);
+
+// circleFunc3.add(fxSvg301, fxSvg302);
+
+// circleFunc4.add(fxSvg401, fxSvg402);
+
+// horizontalFunc1.add(leftSvg101, leftSvg102);
+
+// horizontalFunc2.add(rightSvg101, rightSvg102);
+
+// verticalFunc1.add(topSvg101, topSvg102);
+
+// verticalFunc2.add(bottomSvg101, bottomSvg102);
+
+// // Each completed layer added to the main application stage/canvas
+
+// plutoneContainer.add(
+//   fx1Layer,
+//   fx2Layer,
+//   fx3Layer,
+//   fx4Layer,
+//   centerLayer,
+//   leftLayer,
+//   topLayer,
+//   bottomLayer,
+//   rightLayer
+// );
