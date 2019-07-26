ea4a96dc4a442602592172495464651699cfd50d
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-26 17:37:08 +0200

    first attempt at rendering shard component with konva

--- src/components/Shard/index.js
+++ src/components/Shard/index.js
@@ -1,65 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Tone from 'tone';
+import { Layer, Path } from 'react-konva';
 import styles from './Shard.module.scss';
-import Center from './Images/center.svg';
+// import Center from './Images/center.svg';
 
-const Shard = ({ shardTrack, instArray, instIndex }) => {
+const Shard = ({ shardTrack, instArray, instIndex, shape }) => {
+  console.log(shape[instIndex]);
   const [shardVolume, setShardVolume] = useState(0);
   const [isKickMain, setKickMainValue] = useState(false);
 
-  const handleChange = event => {
-    const toNumber = parseFloat(event.target.value);
-    setShardVolume(toNumber);
-    shardTrack.get(instArray[instIndex]).volume.value = toNumber;
-  };
+  //   const handleChange = event => {
+  //     const toNumber = parseFloat(event.target.value);
+  //     setShardVolume(toNumber);
+  //     shardTrack.get(instArray[instIndex]).volume.value = toNumber;
+  //   };
 
-  useEffect(() => {
-    if (instArray[instIndex] === 'kickMain') {
-      setKickMainValue(true);
-      shardTrack.get(instArray[instIndex]).volume.value = 85;
-    }
-    // only using this value once
-    // eslint-disable-next-line
-  }, []);
+  //   useEffect(() => {
+  //     if (instArray[instIndex] === 'kickMain') {
+  //       setKickMainValue(true);
+  //       shardTrack.get(instArray[instIndex]).volume.value = 85;
+  //     }
+  //     // only using this value once
+  //     // eslint-disable-next-line
+  //   }, []);
 
-  useEffect(() => {
-    Tone.Transport.start();
-    Tone.Transport.bpm.value = 92;
-    const track = shardTrack.get(instArray[instIndex]);
-    // eslint-disable-next-line no-unused-vars
-    const loop = new Tone.Loop(time => {
-      track.start();
-    }, '8m').start(0);
-  }, [shardTrack, instArray, instIndex]);
-
+  //   useEffect(() => {
+  //     Tone.Transport.start();
+  //     Tone.Transport.bpm.value = 92;
+  //     const track = shardTrack.get(instArray[instIndex]);
+  //     // eslint-disable-next-line no-unused-vars
+  //     const loop = new Tone.Loop(time => {
+  //       track.start();
+  //     }, '8m').start(0);
+  //   }, [shardTrack, instArray, instIndex]);
+  const shard = shape[instIndex];
+  console.log(shard.name);
   return (
     <>
-      <div className={styles.slideContainer}>
-        {isKickMain ? (
-          <input
-            className={styles.kickMain}
-            defaultValue={shardVolume}
-            type="image"
-            alt="center"
-            src={Center}
-            min="0"
-            max="85"
-            onChange={handleChange}
-            step="2"
-          />
-        ) : (
-          <input
-            className={styles.slider}
-            defaultValue={shardVolume}
-            type="range"
-            min="0"
-            max="85"
-            onChange={handleChange}
-            step="2"
-          />
-        )}
-      </div>
+      <Layer draggable>
+        {shard.map(path => {
+          return (
+            <Path
+              x={path.x}
+              y={path.y}
+              data={path.data}
+              fill={path.fill}
+              stroke={path.stroke}
+              strokeWidth={path.strokeWidth}
+              scale={path.scale}
+            />
+          );
+        })}
+      </Layer>
     </>
   );
 };
