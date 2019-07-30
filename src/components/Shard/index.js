8b0fe43a48ff4b743dc1f5823cb98d6302f7665a
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-30 15:49:37 +0200

    adding refs to move the shards

--- src/components/Shard/index.js
+++ src/components/Shard/index.js
@@ -3,8 +3,13 @@ import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Tone from 'tone';
 import { Group, Layer, Path } from 'react-konva';
-import Konva from 'konva';
-import { moveShard1 } from '../../context/moving-shapes';
+import {
+  moveBassMainBottom,
+  moveDrumsMainTop,
+  moveKeysMainLeft,
+  moveSynthStrRight,
+  moveKickMainCenter,
+} from '../../context/moving-shapes';
 // import styles from './Shard.module.scss';
 
 const Shard = ({ shardTrack, instrumentName, instrumentIndex, shapeObject }) => {
@@ -15,6 +20,7 @@ const Shard = ({ shardTrack, instrumentName, instrumentIndex, shapeObject }) => 
   const [isY, setY] = useState(null);
   const inputEL = useRef(null);
   const layerEL = useRef(null);
+  const [isDrag, setDrag] = useState(false);
   // Array of objects with the paths for each layer
   const shard = shapeObject[instrumentIndex];
   const maxVolume = 70;
@@ -26,12 +32,25 @@ const Shard = ({ shardTrack, instrumentName, instrumentIndex, shapeObject }) => 
       setKickMainValue(true);
       shardTrack.get(instrumentName).volume.value = maxVolume;
     }
-
     // eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    moveShard1(inputEL.current, layerEL.current);
+    if (instrumentName === 'drumsMain') {
+      moveDrumsMainTop(layerEL.current, isDrag);
+    }
+    if (instrumentName === 'synthStr') {
+      moveSynthStrRight(layerEL.current);
+    }
+    if (instrumentName === 'bassMain') {
+      moveBassMainBottom(layerEL.current);
+    }
+    if (instrumentName === 'keysMain') {
+      moveKeysMainLeft(layerEL.current);
+    }
+    // if (instrumentName === 'kickMain') {
+    //   moveKickMainCenter(layerEL.current);
+    // }
   }, []);
 
   useEffect(() => {
@@ -105,36 +124,43 @@ const Shard = ({ shardTrack, instrumentName, instrumentIndex, shapeObject }) => 
 
   return (
     <>
-      <Layer
-        ref={layerEL}
-        draggable
-        dragBoundFunc={pos => {
-          let coordinates = null;
-          if (instrumentName !== 'kickMain') {
-            getSoundHandler(instrumentName)(pos);
-            coordinates = {
-              x: isX,
-              y: isY,
-            };
-          }
-          return coordinates;
-        }}
-      >
-        {shard.map(({ x, y, data, fill, stroke, strokeWidth, scale }, index) => {
-          return (
-            <Path
-              ref={inputEL}
-              key={instrumentName + index}
-              x={x}
-              y={y}
-              data={data}
-              fill={fill}
-              stroke={stroke}
-              strokeWidth={strokeWidth}
-              scale={scale}
-            />
-          );
-        })}
+      <Layer ref={layerEL}>
+        <Group
+          draggable
+          dragBoundFunc={pos => {
+            let coordinates = null;
+            if (instrumentName !== 'kickMain') {
+              getSoundHandler(instrumentName)(pos);
+              coordinates = {
+                x: isX,
+                y: isY,
+              };
+            }
+            return coordinates;
+          }}
+          onDragMove={() => {
+            setDrag(true);
+          }}
+          onDragEnd={() => {
+            setDrag(false);
+          }}
+        >
+          {shard.map(({ x, y, data, fill, stroke, strokeWidth, scale }, index) => {
+            return (
+              <Path
+                ref={inputEL}
+                key={instrumentName + index}
+                x={x}
+                y={y}
+                data={data}
+                fill={fill}
+                stroke={stroke}
+                strokeWidth={strokeWidth}
+                scale={scale}
+              />
+            );
+          })}
+        </Group>
       </Layer>
     </>
   );
