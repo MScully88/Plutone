99f065498aba5cb0572b09285e2f4a5008b0b58e
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-07-31 16:49:08 +0200

    import the rest of the sounds

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Tone from 'tone';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import baseStartMain from './Sounds/Plutone_V2_16072019 baseStart.m4a';
+import solo from './Sounds/Plutone_V2_16072019 solo.m4a';
 import bassMain from './Sounds/Plutone_V2_16072019 bass.m4a';
 import synthStr from './Sounds/Plutone_V2_16072019 synthStr.m4a';
 import keysMain from './Sounds/Plutone_V2_16072019 keys.m4a';
 import kickMain from './Sounds/Plutone_V2_16072019 kick.m4a';
 import drumsMain from './Sounds/Plutone_V2_16072019 drums.m4a';
-import fxMain from './Sounds/Plutone_V2_16072019 FX.m4a';
+import fx1 from './Sounds/Plutone_V2_16072019 FX.m4a';
+import fx2 from './Sounds/Plutone_V2_16072019 FX2.m4a';
 import Home from './Pages/Home';
 import Plutone from './Pages/Plutone';
 import Loading from './components/Loading';
 import './App.module.scss';
-import { SoundContextProvider } from './context/sound-context';
 
 const App = props => {
   const [areTracksLoading, setTracksLoading] = useState(true);
@@ -24,13 +25,15 @@ const App = props => {
   useEffect(() => {
     const tracks = new Tone.Players(
       {
-        // baseStartMain,
-        bassMain,
-        synthStr,
-        // fxMain,
-        keysMain,
-        drumsMain,
-        kickMain,
+        drumsMain, // top
+        solo, // topR
+        synthStr, // right
+        fx1, // bottomR
+        bassMain, // bottom
+        fx2, // bottomL
+        keysMain, // left
+        baseStartMain, // topL
+        kickMain, // centre
       },
       {
         volume: -90,
