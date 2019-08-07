2ccb7e39e79bffd5eb74ec102cec8064ae9ef87b
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-07 14:39:49 +0200

    adding handlers to start sound fx

--- src/components/Moon/index.js
+++ src/components/Moon/index.js
@@ -1,8 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
+import uuid from 'uuid/v1';
+import Tone from 'tone';
 import { Layer, Path, Group } from 'react-konva';
 import { filterTopL, flangerTopR, filterBottomR, flangerBottomL } from '../../helpers/moving-moons';
+import Shard from '../Shard';
 
-const Moon = ({ moon, moonName }) => {
+const Moon = ({ moon, moonName, shardTrack }) => {
+  const [filter, setFilter] = useState(null);
+  const [wetPingPong, setWetPingPong] = useState(0);
+
+  // useEffect(() => {
+  //   // const pingPong = new Tone.PingPongDelay('4n', 0.5).toMaster().start();
+  //   // const autoWah = new Tone.AutoWah(50, 6, -30).toMaster().start();
+  //   // // initialize the synth and connect to autowah
+  //   // autoWah.wet.value = wetPingPong;
+  //   // shardTrack.chain(autoWah);
+  //   const pingPong = new Tone.AutoWah(50, 6, -30).toMaster();
+  //   pingPong.wet.value = wetPingPong;
+  //   // const pingPong = new Tone.Effect();
+  //   const drum = shardTrack.connect(pingPong);
+  // }, [wetPingPong]);
+
   const layerEL = useRef(null);
   useEffect(() => {
     if (moonName === 'filterTopL') {
@@ -18,6 +36,52 @@ const Moon = ({ moon, moonName }) => {
       flangerBottomL(layerEL.current);
     }
   }, []);
+
+  const handlefilterTopL = ({ x, y }) => {
+    const filterX = Math.abs(x) * 0.01;
+    if (x < 1) {
+      setWetPingPong(filterX);
+    }
+  };
+
+  const handleflangerTopR = ({ x, y }) => {
+    const filterX = Math.abs(x) * 0.01;
+    if (x < 1) {
+      setWetPingPong(filterX);
+    }
+  };
+  
+  const handlefilterBottomR = ({ x, y }) => {
+    const filterX = Math.abs(x) * 0.01;
+    if (x < 1) {
+      setWetPingPong(filterX);
+    }
+  };
+
+  const handleflangerBottomL = ({ x, y }) => {
+    const filterX = Math.abs(x) * 0.01;
+    if (x < 1) {
+      setWetPingPong(filterX);
+    }
+  };
+
+  const getMoonHandler = moonName => {
+    let handler = null;
+    if (moonName === 'filterTopL') {
+      handler = handlefilterTopL;
+    }
+    if (moonName === 'flangerTopR') {
+      handler = handleflangerTopR;
+    }
+    if (moonName === 'filterBottomR') {
+      handler = handlefilterBottomR;
+    }
+    if (moonName === 'flangerBottomL') {
+      handler = handleflangerBottomL;
+    }
+    return handler;
+  };
+
   return (
     <Layer ref={layerEL}>
       <Group
@@ -27,6 +91,7 @@ const Moon = ({ moon, moonName }) => {
           const y = 0;
           const radius = 25;
           const scale = radius / Math.sqrt(Math.pow(pos.x - x, 2) + Math.pow(pos.y - y, 2));
+          getMoonHandler(moonName)(pos);
           if (scale < 1) {
             return {
               y: Math.round((pos.y - y) * scale + y),
@@ -39,7 +104,7 @@ const Moon = ({ moon, moonName }) => {
         {moon.map(({ x, y, data, fill, stroke, strokeWidth, scale }, index) => {
           return (
             <Path
-              key={`${index}_MOON`}
+              key={uuid(index)}
               x={x}
               y={y}
               data={data}
