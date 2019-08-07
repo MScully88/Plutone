93fe373810264430d77d04a76c42ed43777dbbda
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-07 14:41:01 +0200

    reducing movements of the moon

--- src/helpers/moving-moons.js
+++ src/helpers/moving-moons.js
@@ -2,9 +2,9 @@ import Konva from 'konva';
 
 // Circular Movement Functionality & Floating Animation Fx1
 const filterTopL = layer => {
-  const amplitude = 25;
+  const amplitude = 12;
   const period = 10000; // in milliseconds
-  const centerX = 78;
+  const centerX = 0;
   const animFx1 = new Konva.Animation(frame => {
     layer.x(amplitude * Math.sin((frame.time * 2 * Math.PI) / period) + centerX);
     layer.y(amplitude * Math.sin((frame.time * 2 * Math.PI) / period) + centerX);
@@ -15,9 +15,9 @@ const filterTopL = layer => {
 // Circular Movement Functionality & Floating Animation Fx2
 
 const flangerTopR = layer => {
-  const amplitude2 = 29;
+  const amplitude2 = 12;
   const period2 = 13000; // in milliseconds
-  const centerX2 = 58;
+  const centerX2 = 0;
   const animFx2 = new Konva.Animation(frame => {
     layer.x(amplitude2 * Math.sin((frame.time * 2 * Math.PI) / period2) + centerX2);
     layer.y(amplitude2 * Math.sin((frame.time * 2 * Math.PI) / period2) + centerX2);
@@ -27,9 +27,9 @@ const flangerTopR = layer => {
 
 // Circular Movement Functionality & Floating Animation Fx3
 const filterBottomR = layer => {
-  const amplitude3 = 29;
+  const amplitude3 = 10;
   const period3 = 10000;
-  const centerX3 = 58;
+  const centerX3 = 0;
   const animFx3 = new Konva.Animation(frame => {
     layer.x(amplitude3 * Math.sin((frame.time * 2 * Math.PI) / period3) + centerX3);
     layer.y(amplitude3 * Math.sin((frame.time * 2 * Math.PI) / period3) + centerX3);
@@ -40,9 +40,9 @@ const filterBottomR = layer => {
 // Circular Movement Functionality & Floating Animation Fx4
 
 const flangerBottomL = layer => {
-  const amplitude4 = 30;
+  const amplitude4 = 10;
   const period4 = 7000;
-  const centerX4 = 58;
+  const centerX4 = 0;
   const animFx4 = new Konva.Animation(frame => {
     layer.x(amplitude4 * Math.sin((frame.time * 2 * Math.PI) / period4) + centerX4);
     layer.y(amplitude4 * Math.sin((frame.time * 2 * Math.PI) / period4) + centerX4);
