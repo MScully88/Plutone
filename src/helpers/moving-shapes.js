6cc7f6695f109ad2cf8c3d263c2b3fdc12514bad
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-08 14:33:00 +0200

    making sure the right layer is targeted

--- src/helpers/moving-shapes.js
+++ src/helpers/moving-shapes.js
@@ -10,7 +10,7 @@ const moveKeysMainLeft = layer => {
   const animLeft = new Konva.Animation(frame => {
     layer.y(amplitudeLeft * Math.sin((frame.time * 2 * Math.PI) / periodLeft) + centerXLeft);
     layer.x(amplitudeLeft * Math.sin((frame.time * 2 * Math.PI) / periodLeft) + centerXLeft);
-  }, layer);
+  }, layer.parent);
   animLeft.start();
 };
 
@@ -21,26 +21,19 @@ const moveSynthStrRight = layer => {
   const centerXRight = 8.73;
   const animRight = new Konva.Animation(frame => {
     layer.x(amplitudeRight * Math.sin((frame.time * 2 * Math.PI) / periodRight) + centerXRight);
-  }, layer);
+  }, layer.parent);
   animRight.start();
 };
 
 // // Top
-const moveDrumsMainTop = (layer, isDrag) => {
+const moveDrumsMainTop = layer => {
   const amplitudeTop = 10;
   const periodTop = 13000;
   const centerXTop = 8.75;
   const animTop = new Konva.Animation(frame => {
     layer.y(amplitudeTop * Math.sin((frame.time * 2 * Math.PI) / periodTop) + centerXTop);
-  }, layer);
-  function animation() {
-    if (!isDrag) {
-      animTop.start();
-    } else {
-      animTop.stop();
-    }
-  }
-  animation();
+  }, layer.parent);
+  animTop.start();
 };
 
 // // Bottom
@@ -50,7 +43,7 @@ const moveBassMainBottom = layer => {
   const centerXBottom = 8.75;
   const animBottom = new Konva.Animation(frame => {
     layer.y(amplitudeBottom * Math.sin((frame.time * 2 * Math.PI) / periodBottom) + centerXBottom);
-  }, layer);
+  }, layer.parent);
 
   animBottom.start();
 };
@@ -60,7 +53,7 @@ const moveKickMainCenter = layer => {
   const animCenterRotation = new Konva.Animation(frame => {
     const angleDiff = (frame.timeDiff * angularSpeed) / 1000;
     layer.rotate(angleDiff);
-  }, layer);
+  }, layer.parent);
 
   animCenterRotation.start();
 };
