0ca3455754eba06c7546c4180ec5e93a72c7ec36
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-09 12:11:21 +0200

    storing adjust width in another file for future ref

--- src/helpers/helperFuncs.js
+++ src/helpers/helperFuncs.js
@@ -15,3 +15,30 @@ const shuffle = array => {
 };
 
 export default { shuffle };
+
+// code to adjust the height and width of the container
+
+// const [containerScale, setContainerScale] = useState(null);
+// const fitStageIntoParentContainer = () => {
+//   const CANVAS_VIRTUAL_WIDTH = 780;
+//   const CANVAS_VIRTUAL_HEIGHT = 820;
+//   // now you may want to make it visible even on small screens
+//   // we can just scale it
+//   const scale = Math.min(
+//     window.innerWidth / CANVAS_VIRTUAL_WIDTH,
+//     window.innerHeight / CANVAS_VIRTUAL_HEIGHT,
+//   );
+//   setContainerScale(scale);
+// };
+
+// const toggle = () => {
+//   if (window.innerWidth < 780) {
+//     fitStageIntoParentContainer();
+//   }
+// };
+
+// useEffect(() => {
+//   window.addEventListener('resize', () => {
+//     toggle();
+//   });
+// });
