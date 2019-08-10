e180ca0c6595685ee4915e7d72485dcc071f9348
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-10 13:13:49 +0200

    deleting unneccesary files

--- src/helpers/helperFuncs.js
+++ src/helpers/helperFuncs.js
@@ -1,21 +1,3 @@
-const shuffle = array => {
-  // initial shuffle of quotes //
-  let i = array.length;
-  let j = 0;
-  let temp;
-
-  while (i--) {
-    j = Math.floor(Math.random() * (i + 1));
-
-    temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-};
-
-export default { shuffle };
-
 // code to adjust the height and width of the container
 
 // const [containerScale, setContainerScale] = useState(null);
