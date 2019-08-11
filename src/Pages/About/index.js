1fd25dbdd92958504892eabd7eb3a26b6c321226
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-08-11 13:57:35 +0200

    fixing errors and adding more to the about page

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -4,15 +4,27 @@ import styles from './about.module.scss';
 
 const About = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <nav>
         <Link to="/">Home</Link>
       </nav>
       <h1>About</h1>
+      <quote>
+        “The internet can seem like a mall at times, we need more spaces like parks and oceans”. –
+        Ruth John
+      </quote>
       <section>
-        <p>Plutone is a project created by Michael and Claudio.</p>
+        <h2>What is it?</h2>
+        <p>
+          Plutone is a project created by Michael and Claudio.Plutone is an experiment that aims to
+          bring audio and visuals together, creating an interactive soundscape experience through
+          the browser. The musical planet Plutone has been split into mutliple pieces and has been
+          spread throughout space. Bring all of the pieces back together to return Plutone to its
+          former glory.
+        </p>
       </section>
       <section>
+        <h2>The Team</h2>
         <h2>Claudio Tocco</h2>
         <h2>Michael Scully</h2>
       </section>
