5fd18a6e329499cf63dfa2c3b1e964e6617614fb
MScully88 <michael.scully88@gmail.com>
2019-08-13 20:51:53 +0200

    Updates

---
+++ precache-manifest.9535796ee045b490e06f9113d6525cbd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be868a3af30d6c6f4682d1a4830a5be",
+    "url": "/Plutone/public/index.html/index.html"
+  },
+  {
+    "revision": "661bbf927c5e0452ab4f",
+    "url": "/Plutone/public/index.html/static/css/main.9a53673b.chunk.css"
+  },
+  {
+    "revision": "5ef906233d8ada3c7799",
+    "url": "/Plutone/public/index.html/static/js/2.252d363c.chunk.js"
+  },
+  {
+    "revision": "661bbf927c5e0452ab4f",
+    "url": "/Plutone/public/index.html/static/js/main.20c7c700.chunk.js"
+  },
+  {
+    "revision": "02c617d3e3c664e47f60",
+    "url": "/Plutone/public/index.html/static/js/runtime~main.0acfa9b3.js"
+  },
+  {
+    "revision": "584122a82a3b358d083afc42ea7f2d42",
+    "url": "/Plutone/public/index.html/static/media/LowAmb_V3.584122a8.mp3"
+  },
+  {
+    "revision": "3ef2ca93abccbaaf0dfd59957f5648d7",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 FX.3ef2ca93.m4a"
+  },
+  {
+    "revision": "c99a561cc42c73be5cc05ea74b7576ca",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 baseStart.c99a561c.m4a"
+  },
+  {
+    "revision": "0aa747665b405dbffff9fc17e06307a6",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 bass.0aa74766.m4a"
+  },
+  {
+    "revision": "9dbd27024646eb902e05b97419b414a1",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 drums.9dbd2702.m4a"
+  },
+  {
+    "revision": "0b2c68f1cee0620c6b3f8b0f9a23ca80",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 keys.0b2c68f1.m4a"
+  },
+  {
+    "revision": "245d9d01b0d2546d14e090b9d962e7c1",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 kick.245d9d01.m4a"
+  },
+  {
+    "revision": "47441fa4f15f86e7664bf6bd56e68ba7",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V2_16072019 synthStr.47441fa4.m4a"
+  },
+  {
+    "revision": "a2e3ec9e8fe9fbb9869046aeeefc7a9e",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V3_05082019 bassSolo.a2e3ec9e.m4a"
+  },
+  {
+    "revision": "53a7809833c47dedeb8f8331eb7c6c95",
+    "url": "/Plutone/public/index.html/static/media/Plutone_V3_05082019 fx2.53a78098.m4a"
+  },
+  {
+    "revision": "26a9a69410eb1883ca207d65d40d22b8",
+    "url": "/Plutone/public/index.html/static/media/claudio.26a9a694.jpg"
+  },
+  {
+    "revision": "4426bba0a99abf09a9a7d0c54cb061eb",
+    "url": "/Plutone/public/index.html/static/media/logo.4426bba0.svg"
+  },
+  {
+    "revision": "be4be8c925bf4d0e3b0e85b00fdbd626",
+    "url": "/Plutone/public/index.html/static/media/michael.be4be8c9.jpg"
+  }
+]);
