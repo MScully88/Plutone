ac72bbb608b06f22b550149b210833ce9f561986
MScully88 <michael.scully88@gmail.com>
2019-08-14 10:24:24 +0200

    Updates

---
+++ precache-manifest.f51ff72af7d49d3f93b65d39f89cbd73.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a04edb8927ea93f2dffa2eb360f315a",
+    "url": "/Plutone/index.html"
+  },
+  {
+    "revision": "5371dca783ccca9236ec",
+    "url": "/Plutone/static/css/main.9a53673b.chunk.css"
+  },
+  {
+    "revision": "b73aaf245396bc133145",
+    "url": "/Plutone/static/js/2.beb167ec.chunk.js"
+  },
+  {
+    "revision": "5371dca783ccca9236ec",
+    "url": "/Plutone/static/js/main.9ff16a3c.chunk.js"
+  },
+  {
+    "revision": "453ba2ebeb05d18e820a",
+    "url": "/Plutone/static/js/runtime~main.847df09e.js"
+  },
+  {
+    "revision": "584122a82a3b358d083afc42ea7f2d42",
+    "url": "/Plutone/static/media/LowAmb_V3.584122a8.mp3"
+  },
+  {
+    "revision": "3ef2ca93abccbaaf0dfd59957f5648d7",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 FX.3ef2ca93.m4a"
+  },
+  {
+    "revision": "c99a561cc42c73be5cc05ea74b7576ca",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 baseStart.c99a561c.m4a"
+  },
+  {
+    "revision": "0aa747665b405dbffff9fc17e06307a6",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 bass.0aa74766.m4a"
+  },
+  {
+    "revision": "9dbd27024646eb902e05b97419b414a1",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 drums.9dbd2702.m4a"
+  },
+  {
+    "revision": "0b2c68f1cee0620c6b3f8b0f9a23ca80",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 keys.0b2c68f1.m4a"
+  },
+  {
+    "revision": "245d9d01b0d2546d14e090b9d962e7c1",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 kick.245d9d01.m4a"
+  },
+  {
+    "revision": "47441fa4f15f86e7664bf6bd56e68ba7",
+    "url": "/Plutone/static/media/Plutone_V2_16072019 synthStr.47441fa4.m4a"
+  },
+  {
+    "revision": "a2e3ec9e8fe9fbb9869046aeeefc7a9e",
+    "url": "/Plutone/static/media/Plutone_V3_05082019 bassSolo.a2e3ec9e.m4a"
+  },
+  {
+    "revision": "53a7809833c47dedeb8f8331eb7c6c95",
+    "url": "/Plutone/static/media/Plutone_V3_05082019 fx2.53a78098.m4a"
+  },
+  {
+    "revision": "26a9a69410eb1883ca207d65d40d22b8",
+    "url": "/Plutone/static/media/claudio.26a9a694.jpg"
+  },
+  {
+    "revision": "4426bba0a99abf09a9a7d0c54cb061eb",
+    "url": "/Plutone/static/media/logo.4426bba0.svg"
+  },
+  {
+    "revision": "be4be8c925bf4d0e3b0e85b00fdbd626",
+    "url": "/Plutone/static/media/michael.be4be8c9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98cb26ca28ff94c2b719676098586582.js"
+  "/Plutone/precache-manifest.f51ff72af7d49d3f93b65d39f89cbd73.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Plutone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
